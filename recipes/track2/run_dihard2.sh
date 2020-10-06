b1d5d9d934d11d57187a608c229bc1ffacafada0
Neville Ryant <nryant@gmail.com>
2020-10-06 19:30:04 -0400

    FEAT: First working supervised SAD for DIHARD II.

--- recipes/track2/run_dihard2.sh
+++ recipes/track2/run_dihard2.sh
@@ -11,8 +11,18 @@ nj=40
 decode_nj=40
 PYTHON=python
 stage=0
-sad_stage=0
+sad_train_stage=0
+sad_decode_stage=0
 diarization_stage=0
+
+# If following is True, then SAD output will be evaluated against reference
+# following decoding stage. This step requires the following Python packages be
+# installed:
+#
+# - pyannote.core
+# - pyannote.metrics
+# - pandas
+eval_sad=true
 
 
 #####################################
@@ -65,3 +75,47 @@ fi
 #####################################
 # Train SAD system.
 #####################################
+if [ $stage -le 1 ]; then
+    echo "$0: Training SAD..."
+    local/train_sad.sh \
+	--nj $nj --stage $sad_train_stage \
+	data/dihard2_dev exp/sad_tdnn_stats
+fi
+
+
+#####################################
+# SAD decoding.
+#####################################
+if [ $stage -le 2 ]; then
+    echo "$0: Applying SAD model to DEV/EVAL..."
+    for dset in dev eval; do
+	local/segmentation/detect_speech_activity.sh \
+	    --nj $nj --stage $sad_decode_stage \
+	    data/dihard2_${dset} exp/sad_tdnn_stats \
+	    mfcc exp/sad_tdnn_stats_decode_${dset} \
+       	    data/dihard2_${dset}_seg
+    done
+fi
+
+
+#####################################
+# Evaluate SAD output.
+#####################################
+if [ $stage -le 3  -a  $eval_sad = "true" ]; then
+    echo "$0: Scoring SAD output on DEV set..."
+    local/segmentation/score_sad.py \
+	--n-jobs $nj --collar 0.0\
+	data/dihard2_dev/segments \
+	data/dihard2_dev_seg/segments \
+	local/dihard2_dev_recordings.tsv
+    echo ""
+    echo ""
+
+    echo "$0: Scoring SAD output on EVAL set..."
+    local/segmentation/score_sad.py \
+        --n-jobs $nj --collar 0.0\
+        data/dihard2_eval/segments \
+        data/dihard2_eval_seg/segments \
+        local/dihard2_eval_recordings.tsv
+
+fi
