207297a1ef1e243746c42783ed99f848b6fa5bd1
Neville Ryant <nryant@gmail.com>
2020-10-13 16:46:54 -0400

    FEAT: Tune hyperparameters in decoding script.

--- recipes/track2/local/segmentation/detect_speech_activity.sh
+++ recipes/track2/local/segmentation/detect_speech_activity.sh
@@ -4,65 +4,110 @@
 #              2017  Nagendra Kumar Goel
 # Apache 2.0.
 
-# This script does nnet3-based speech activity detection given an input 
+# This script does nnet3-based speech activity detection given an input
 # kaldi data directory and outputs a segmented kaldi data directory.
-# This script can also do music detection and other similar segmentation
-# using appropriate options such as --output-name output-music.
 #
 # Adapted from:
 #
 #     wsj/s5/steps/segmentation/detect_speech_activity.sh
-set -e 
+set -e
 set -o pipefail
 set -u
 
 if [ -f ./path.sh ]; then . ./path.sh; fi
 
-nj=32
-cmd=run.pl
+###############################################################################
+# Global config
+###############################################################################
 stage=-1
+cmd=run.pl
+nj=40
 
-# Feature options (Must match training)
-mfcc_config=conf/mfcc_hires.conf
 
-# SAD network config
-iter=final  # Model iteration to use
-
-# Contexts must ideally match training for LSTM models, but
-# may not necessarily for stats components
-extra_left_context=0  # Set to some large value, typically 40 for LSTM (must match training)
+###############################################################################
+# Context config
+###############################################################################
+# TDNN already has such a wide context window, the values set here have next to
+# no impact for performance.
+extra_left_context=0
 extra_right_context=0  
 extra_left_context_initial=-1
 extra_right_context_final=-1
 frames_per_chunk=150
 
-# Decoding options
-graph_opts="--min-silence-duration=0.030 --min-speech-duration=0.240 --max-speech-duration=60.0"
+
+###############################################################################
+# NNet config
+###############################################################################
+# Use final model.
+iter=final
+
+
+###############################################################################
+# Decoding config
+###############################################################################
+# These parameters control the construction of the decoding graph and can be
+# used to prevent Viterbi decoding from ever outputting speech or non-speech
+# segments with durations below some threshold. However, these parameters have
+# no impact on performance unless set to extreme values such as minimum
+# non-speech duration of 1 second or minimum speech duration of 2 seconds.
+min_sil_dur=0.030     # Minimum allowed duration in seconds of non-speech
+                      # segments.
+min_speech_dur=0.240  # Minimum allowed duration in seconds of speech segments.
+max_speech_dur=60.0   # Maximum allowed duration in seconds of speech segments.
+
+# Scaling factor for acoustic likelihoods.
 acwt=0.3
 
-# These <from>_in_<to>_weight represent the fraction of <from> probability 
-# to transfer to <to> class.
-# e.g. --speech-in-sil-weight=0.0 --garbage-in-sil-weight=0.0 --sil-in-speech-weight=0.0 --garbage-in-speech-weight=0.3
-transform_probs_opts=""
+# Scaling factor for speech likelihood. Values > 1 will increase false alarm rate
+# and reduces miss rate. Values < 1 will increase miss rate and decrease false
+# alarm rate.
+speech_weight=1.0
 
-# Postprocessing options
-segment_padding=0.0   # Duration (in seconds) of padding added to segments 
-min_segment_dur=0   # Minimum duration (in seconds) required for a segment to be included
-                    # This is before any padding. Segments shorter than this duration will be removed.
-                    # This is an alternative to --min-speech-duration above.
-merge_consecutive_max_dur=0   # Merge consecutive segments as long as the merged segment is no longer than this many
-                              # seconds. The segments are only merged if their boundaries are touching.
-                              # This is after padding by --segment-padding seconds.
-                              # 0 means do not merge. Use 'inf' to not limit the duration.
+graph_opts="--min-silence-duration=${min_sil_dur} --min-speech-duration=${min_speech_dur} --max-speech-duration=${max_speech_dur}"
 
-echo $* 
 
+###############################################################################
+# Postprocessing config
+###############################################################################
+# All speech segments will be padded on either side by up to this many seconds.
+# If extending the segment onset/offset by the full padding duration would result
+# in the segment extending past the edge of the recording or overlapping an
+# adjacent speech segment, then it will be truncated.
+# If exte
+segment_padding=0.0
+
+# Minimum duration in seconds of speech segments. Any segments shorter than this
+# threshold will be eliminated PRIOR to the padding step.
+min_segment_dur=0
+
+# Max duration in seconds of merged segments. Overlapping speech segments will be
+# merged by an iterable process as long as the duration of the merged segment
+# does not exceed this threshold. There are two special cases:
+# - 0: perform no merging
+# - inf: no limit on duration of merged segments
+merge_consecutive_max_dur=inf
+
+
+##############################################################################
+# Parse options, etc.
+##############################################################################
+echo $*
+
+set -e
+set -o pipefail
+set -u
+
+if [ -f ./path.sh ]; then . ./path.sh; fi
 . utils/parse_options.sh
 
 if [ $# -ne 5 ]; then
-  echo "This script does nnet3-based speech activity detection given an input kaldi "
-  echo "data directory and outputs an output kaldi data directory."
-  echo "See script for details of the options to be supplied."
+  echo "This script does nnet3-based speech activity detection given an input "
+  echo "Kaldi data directory and outputs a new Kaldi data directory where the "
+  echo "segments file contains the results of SAD."
+  echo ""
+  echo "Main options are documeted below; see script for details of the other "
+  echo "options."
   echo "Usage: $0 <src-data-dir> <sad-nnet-dir> <mfcc-dir> <work-dir> <out-data-dir>"
   echo " e.g.: $0 ~/workspace/egs/ami/s5b/data/sdm1/dev exp/nnet3_sad_snr/nnet_tdnn_j_n4 \\"
   echo "    mfcc_hires exp/segmentation_sad_snr/nnet_tdnn_j_n4 data/ami_sdm1_dev"
@@ -71,9 +116,9 @@ if [ $# -ne 5 ]; then
   echo "  --cmd (utils/run.pl|utils/queue.pl <queue opts>) # how to run jobs."
   echo "  --nj <num-job>                                 # number of parallel jobs to run."
   echo "  --stage <stage>                                # stage to do partial re-run from."
-  echo "  --output-name <name>    # The output node in the network"
   echo "  --extra-left-context  <context|0>   # Set to some large value, typically 40 for LSTM (must match training)"
   echo "  --extra-right-context  <context|0>   # For BLSTM or statistics pooling"
+  echo "--speech-weight <weight|1>  # Scaling factor for speech pseudo-likelihood"
   exit 1
 fi
 
@@ -94,11 +139,12 @@ whole_data_dir=data/${data_id}_whole_mfcc_hires
     rm -fr ${whole_data_dir}
     utils/data/convert_data_dir_to_whole.sh $src_data_dir $whole_data_dir
   fi
-  
+
 
 ###############################################################################
-## Extract input features 
-###############################################################################
+## Extract input features
+###############################################################################  
+mfcc_config=conf/mfcc_hires.conf
 if [ $stage -le 1 ]; then
   utils/fix_data_dir.sh $whole_data_dir
   steps/make_mfcc.sh \
@@ -153,7 +199,7 @@ if [ $stage -le 3 ]; then
 
     # 1 for silence and 2 for speech
     cat <<EOF > $graph_dir/words.txt
-<eps> 0	
+<eps> 0
 silence 1
 speech 2
 EOF
@@ -171,24 +217,12 @@ fi
 ## Do Viterbi decoding to create per-frame alignments.
 ###############################################################################
 post_vec=$sad_nnet_dir/post_output.vec
-if [ ! -f $sad_nnet_dir/post_output.vec ]; then
-  if [ ! -f $sad_nnet_dir/post_output.txt ]; then
-    echo "$0: Could not find $sad_nnet_dir/post_output.vec. "
-    echo "Re-run the corresponding stage in the training script possibly "
-    echo "with --compute-average-posteriors=true or compute the priors "
-    echo "from the training labels"
-    exit 1
-  else
-    post_vec=$sad_nnet_dir/post_output.txt
-  fi
-fi
-
 mkdir -p $seg_dir
 if [ $stage -le 4 ]; then
     echo "$0: Running Viterbi decoding..."
-    steps/segmentation/internal/get_transform_probs_mat.py \
-	--priors="$post_vec" \
-	$transform_probs_opts > $seg_dir/transform_probs.mat
+    local/segmentation/get_transform_probs_mat.py \
+        --speech-likelihood-weight $speech_weight  $post_vec \
+	> $seg_dir/transform_probs.mat
     steps/segmentation/decode_sad.sh \
 	--cmd "$cmd" --nj $nj \
 	--acwt $acwt --transform "$seg_dir/transform_probs.mat" \
