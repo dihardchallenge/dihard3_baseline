197a1dac38618c756ecfec22a4ea19692357cd13
LEAP Lab <leap.iisc@gmail.com>
2020-10-30 22:23:32 +0530

    changed script

--- tools/install_kaldi.sh
+++ tools/install_kaldi.sh
@@ -1,61 +1,98 @@
 #!/bin/bash
-# Installation script for Kaldi
-
+# Installation script for Kaldi.
 set -e
 
-SCRIPT_DIR=`realpath $(dirname "$0")`
-NJOBS=40 # Number of parallel jobs to run during build.
-KALDI_GIT=https://github.com/kaldi-asr/kaldi
-KALDI_DIR=`realpath -s $PWD/kaldi`
+# TODO:
+# - More logging.
+# - Clean up kaldi/{src,tools} after successful install by removing temporaries.
+
+#######################
+# Config
+#######################
+NJOBS=20  # Number of parallel jobs for make.
 
 
-# Clone Kaldi and checkout to known working version.
-echo "Installing Kaldi."
-if [ ! -d "$KALDI_DIR" ]; then
-    # Clone Kaldi and checkout to known working version.
-    git clone $KALDI_GIT $KALDI_DIR
-    pushd $KALDI_DIR > /dev/null
-    git checkout 213ae52ac
+#######################
+# Clone repo.
+#######################
+KALDI_REPO=https://github.com/kaldi-asr/kaldi
+KALDI_DIR=$PWD/kaldi
+KALDI_REVISION=ff4cb55a9
 
-    # Perform full Kaldi build with:
-    # - OpenBLAS linkage
-    # - compiled beamformit
-    echo "Building Kaldi."
+if [ ! -d $KALDI_DIR ]; then
+    git clone $KALDI_REPO
+    cd $KALDI_DIR
+    git checkout $KALDI_REVISION
+    cd ..
+fi
 
-    # Prevent Kaldi from switching default python version
-    mkdir -p "tools/python"
-    touch "tools/python/.use_default_python"
 
-    # Build tools.
-    cd tools
-    ./extras/check_dependencies.sh
+#######################
+# Build tools.
+#######################
+cd $KALDI_DIR/tools
+if [ ! -f install.succeeded ]; then
+    # Force Kaldi to use system Python.
+    mkdir -p "python"
+    touch "python/.use_default_python"
+
+    # Compile.
     make -j $NJOBS
-    make -j $NJOBS openblas
-    ./extras/install_beamformit.sh
-    cd ../src
-    ./configure \
-    	--mathlib=OPENBLAS --openblas-root=../tools/OpenBLAS/install
-    make depend -j $NJOBS
+
+    # Compile OpenBLAS. For some reason the default install script doesn't put
+    # components in the places expected by src/configure, so move some things
+    # around.
+    extras/install_openblas.sh
+    mkdir OpenBLAS/lib
+    mv OpenBLAS/*.so OpenBLAS/lib
+    mkdir OpenBLAS/include
+    cp OpenBLAS/*.h OpenBLAS/include
+
+    touch install.succeeded
+
+    # Clean up object files.
+    echo $PWD
+    find .  -type f -name "*.o" -exec rm {} \;
+    find .  -type f -name "*.lo" -exec rm {} \;
+fi
+
+
+#######################
+# Build Kaldi.
+#######################
+cd $KALDI_DIR/src
+if [ ! -f install.succeeded ]; then
+    # Configure.
+    if [ -z "$MKL_ROOT" ]; then
+	# Use default MKL install location if MKL_ROOT not set.
+	MKL_ROOT=/opt/intel/mkl
+    fi
+    if [ -d "$MKL_ROOT" ]; then
+	./configure \
+            --shared --mathlib=MKL --mkl-root=$MKL_ROOT \
+	    --use-cuda=yes
+    else
+	echo "Cannot find MKL library directory. Defaulting to OpenBLAS."
+	echo "If you wish to use MKL:"
+	echo "    - set MKL_ROOT to the directory where MKL was installed "
+	echo "    - run \"rm kaldi/src/install.succeeded\""
+	echo "    - rerun this script"
+	./configure \
+	    --shared \
+	    --mathlib=OPENBLAS --openblas-root=../tools/OpenBLAS/install \
+	    --use-cuda=yes
+    fi
+
+    # Build. May take a while.
+    make -j $NJOBS depend
     make -j $NJOBS
 
-    popd > /dev/null
-else
-  echo "$KALDI_DIR already exists!"
+    # Clean up object files.
+    echo $PWD
+    find .  -type f -name "*.o" -exec rm {} \;
+
+    # Install kaldi_io.
+    pip install kaldi_io
+    
+    touch install.succeeded
 fi
-
-
-# Add config into env.sh
-unset KALDI_DIR
-if [ -s ./env.sh ]; then
-    source env.sh
-fi
-if [ ! -z "${KALDI_DIR}" ]; then
-    echo "KALDI_DIR variable is already in env.sh"
-else
-    echo "Modifying env.sh."
-    echo "export KALDI_DIR=${SCRIPT_DIR}/kaldi" >> env.sh
-    echo "export PATH=\${PATH}:\${KALDI_DIR}" >> env.sh
-fi
-
-
-echo "Successfully installed Kaldi."
