292b9f0ab67c6c91fb9867e78f404f60eaae0ad0
Neville Ryant <nryant@gmail.com>
2020-11-16 13:19:10 -0500

    REFACTOR: Update path.sh to reflect new directory structure.

--- recipes/track1/path.sh
+++ recipes/track1/path.sh
@@ -1,15 +1,10 @@
-export KALDI_ROOT="/state/partition1/softwares/Kaldi_Sept_2020/kaldi"
+export TOOLS_DIR=$PWD/../../tools
+# dscore.
+export PATH=$TOOLS_DIR/dscore:$PATH
+# Kaldi.
+export KALDI_ROOT=$TOOLS_DIR/kaldi/
 [ -f $KALDI_ROOT/tools/env.sh ] && . $KALDI_ROOT/tools/env.sh
-export PATH=$PWD/utils/:$KALDI_ROOT/tools/openfst/bin:$PWD:$PATH:$KALDI_ROOT/src/nnet3bin
+export PATH=$PWD/utils/:$KALDI_ROOT/tools/openfst/bin:$PWD:$PATH
 [ ! -f $KALDI_ROOT/tools/config/common_path.sh ] && echo >&2 "The standard file $KALDI_ROOT/tools/config/common_path.sh is not present -> Exit!" && exit 1
 . $KALDI_ROOT/tools/config/common_path.sh
 export LC_ALL=C
-export PATH="/state/partition1/softwares/Kaldi_Sept_2020/kaldi/src/featbin:$PATH"
-export PATH="/state/partition1/softwares/Kaldi_Sept_2020/kaldi/tools/sph2pipe_v2.5:$PATH"
-export PATH="/state/partition1/softwares/Kaldi_Sept_2020/kaldi/src/bin:$PATH"
-export PATH="/state/partition1/softwares/Kaldi_Sept_2020/kaldi/src/ivectorbin:$PATH"
-LMBIN=$KALDI_ROOT/tools/irstlm/bin
-SRILM=$KALDI_ROOT/tools/srilm/bin/i686-m64
-BEAMFORMIT=$KALDI_ROOT/tools/BeamformIt
-
-export PATH=$PATH:$LMBIN:$BEAMFORMIT:$SRILM
