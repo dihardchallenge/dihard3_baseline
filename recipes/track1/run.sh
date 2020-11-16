f7f586a5116607557440d20f055a73fd5bd9109a
Neville Ryant <nryant@gmail.com>
2020-11-16 13:21:15 -0500

    REFACTOR: Change default command to "run.pl" for local testing.

--- recipes/track1/run.sh
+++ recipes/track1/run.sh
@@ -1,71 +1,67 @@
 #!/bin/bash
 
-set -e
+set -e  # Exit on error.
 
+PYTHON=python  # Python to use; defaults to system Python.
+
+
+#####################################
+# Configuration
+#####################################
 NJOBS=40
-PYTHON=`which python`
-
-#####################################
-#### Set following paths  ###########
-#####################################
-# Path to root of DIHARD II dev release (LDC2019E31).
-#DIHARD_DEV_DIR=/scratch/nryant/dihard2/deliveries/LDC2019E31_Second_DIHARD_Challenge_Development_Data/
-DIHARD_DEV_DIR=/home/prachis/Dihard_2020/LDC2020E12_Third_DIHARD_Challenge_Development_Data/
-
-# Path to root of DIHARD II eval release (LDC2019E32).
-#DIHARD_EVAL_DIR=/scratch/nryant/dihard2/deliveries/LDC2019E32_Second_DIHARD_Challenge_Evaluation_Data_SCRUBBED/
-DIHARD_EVAL_DIR=/data1/SRE18/harshav/dihard_2018/v2/data/DEV_2019/LDC2019E32_Second_DIHARD_Challenge_Evaluation_Data_SCRUBBED/
+stage=0
+diarization_stage=0
 
 
 #####################################
-#### Check deps satisfied ###########
+# Paths to DIHARD III releases
 #####################################
-THIS_DIR=`realpath $(dirname "$0")`
-KALDI_DIR=/state/partition1/softwares/Kaldi_Sept_2020/kaldi
-TOOLS_DIR=$THIS_DIR/../../tools
-SCRIPTS_DIR=$THIS_DIR/../../scripts
-DSCORE_DIR=dscore-master
-# [ -f $TOOLS_DIR/env.sh ] && . $TOOLS_DIR/env.sh
-if [ -z	$KALDI_DIR ]; then
-    echo "KALDI_DIR not defined. Please run tools/install_kaldi.sh"
-    exit 1
+DIHARD_DEV_DIR=/data/working/nryant/dihard3/delivery/builds/LDC2020E12_Third_DIHARD_Challenge_Development_Data
+DIHARD_EVAL_DIR=/data/working/nryant/dihard3/delivery/builds/LDC2020E13_Third_DIHARD_Challenge_Evaluation_Data
+
+
+. ./utils/parse_options.sh
+. ./cmd.sh
+. ./path.sh
+
+
+#####################################
+# Prepare data directories
+#####################################
+if [ $stage -le 0 ]; then
+    echo "$0: Preparing data directories..."
+
+    # dev
+    local/make_data_dir.py \
+	--rttm-dir $DIHARD_DEV_DIR/data/rttm \
+	data/dihard3_dev \
+	$DIHARD_DEV_DIR/data/flac \
+	$DIHARD_DEV_DIR/data/sad
+    utils/utt2spk_to_spk2utt.pl \
+	data/dihard3_dev/utt2spk > data/dihard3_dev/spk2utt
+    ./utils/validate_data_dir.sh \
+	--no-text --no-feats data/dihard3_dev/
+
+    # eval
+    local/make_data_dir.py \
+        --rttm-dir $DIHARD_EVAL_DIR/data/rttm \
+        data/dihard3_eval \
+        $DIHARD_EVAL_DIR/data/flac \
+        $DIHARD_EVAL_DIR/data/sad
+    utils/utt2spk_to_spk2utt.pl \
+        data/dihard3_eval/utt2spk > data/dihard3_eval/spk2utt
+    ./utils/validate_data_dir.sh \
+        --no-text --no-feats data/dihard3_eval/
 fi
-# $SCRIPTS_DIR/prep_eg_dir.sh
+exit 1
 
 
+#####################################                                             # Perform diarization using
+# pretrained model.
 #####################################
-#### Run experiment  ################
-#####################################
-# EG_DIR=$KALDI_DIR/egs/dihard_2018/v2
-# pushd $EG_DIR > /dev/null
-echo $PWD
-
-# Prepare data directory for DEV set.
-echo "Preparing data directory for DEV set..."
-DEV_DATA_DIR=data/dihard_dev_2020_track1
-rm -fr $DEV_DATA_DIR
-local/make_data_dir.py \
-    --rttm-dir $DIHARD_DEV_DIR/data/rttm \
-    $DEV_DATA_DIR \
-    $DIHARD_DEV_DIR/data/flac \
-    $DIHARD_DEV_DIR/data/sad
-utils/fix_data_dir.sh ${DEV_DATA_DIR}
-
-#exit;
-
-# Prepare data directory for EVAL set.
-echo "Preparing data directory for EVAL set...."
-EVAL_DATA_DIR=data/dihard_eval_2020_track1
-rm -fr $EVAL_DATA_DIR
-local/make_data_dir.py \
-   $EVAL_DATA_DIR \
-   $DIHARD_EVAL_DIR/data/flac \
-   $DIHARD_EVAL_DIR/data/sad
-#utils/fix_data_dir.sh $EVAL_DATA_DIR
-
-# Diarize.
-echo "Diarizing..."
-
-./alltracksrun.sh --tracknum 1 --DIHARD_DEV_DIR $DIHARD_DEV_DIR \
+if [ $stage -le 1]; then
+    echo "$0: Diarizing DEV/EVAL..."
+    ./alltracksrun.sh \
+	--tracknum 1 --DIHARD_DEV_DIR $DIHARD_DEV_DIR \
 	--plda_path exp/xvector_nnet_1a/plda_track1 \
 	--DSCORE_DIR $DSCORE_DIR --njobs $NJOBS
