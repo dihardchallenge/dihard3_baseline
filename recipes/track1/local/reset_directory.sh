cfff5e0b7c98fe6d99af4289a5370fd3d5d26eea
Neville Ryant <nryant@gmail.com>
2020-11-23 16:10:19 -0500

    FEAT: Update reset_directory.sh for new structure.

--- recipes/track1/local/reset_directory.sh
+++ recipes/track1/local/reset_directory.sh
@@ -1,5 +1,11 @@
-for name in mfcc exp/*decode* exp/xvector_nnet_1a/xvectors_dihard_*/ exp/xvector_nnet_1a/q exp/xvector_nnet_1a/log exp/xvector_nnet_1a/tuning_track* exp/xvec_init_gauss_1024_ivec_400/dihard* exp/dihard_* exp/make_mfcc ./*.out ./*.err recipes/track*/metrics* data/dihard*; do
-	rm -r $name;
-done;
-
+#!/bin/bash
+# Restore data/ and exp/ directories to virgin state.
+rm -fr data
+for dpath in $(ls -d exp/*/); do
+    dname=$(basename $dpath)
+    if [ $dname == "xvector_nnet_1a" ] || [ $dname == "xvec_init_gauss_1024_ivec_400" ]; then
+	continue
+    fi
+    rm -fr $dpath
+done
 
